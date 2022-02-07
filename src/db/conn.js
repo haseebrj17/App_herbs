12f478c9e6794a85e0d3310332557026724cc5ae
haseebrj17 <56214816+haseebrj17@users.noreply.github.com>
2022-02-07 19:56:01 +0500

    Mongoose connection updated since some options were depracated.

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,11 +1,7 @@
 const mongoose = require('mongoose');
 const key = process.env.DB_CONNECT;
 
-mongoose.connect(key, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-}).then(() => {
+mongoose.connect(key).then(() => {
     console.log("Connected to DB");
 }).catch((err) => {
     console.log("No connection to database\nDue to this error");
